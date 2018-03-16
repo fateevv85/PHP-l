81c6775cd77bd09e67fb98088538e28477387e99
fateevv85 <fateevv85@gmail.com>
2018-03-16 16:23:45 +0300

    finish hw

--- hw1/page1.php
+++ hw1/page1.php
@@ -1,7 +1,23 @@
+<head>
+  <title>Text page</title>
+</head>
 <?php
-/**
- * Created by PhpStorm.
- * User: Василий
- * Date: 16.03.2018
- * Time: 10:47
- */
+date_default_timezone_set("Europe/Moscow");
+$name = 'Vasiliy';
+$age = date('Y') - 1985;
+$currentDate = date('d-m-Y H:i');
+$string = "Меня зовут %s.
+<br>
+Через год мне будет %d лет, а еще через год %d лет.
+<br>
+На моих часах сейчас: %s.";
+
+$text = sprintf($string, $name, $age + 1, $age + 2, $currentDate);
+echo $text;
+echo '<p></p>';
+//replace spaces with underscore
+echo str_replace(' ', '_', $text);
+echo '<p></p>';
+//print substring
+echo strstr($text, 'На моих');
+?>
