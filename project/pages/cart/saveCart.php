0d67029a157a119e2f5b60107283b42f62e46a15
fateevv85 <fateevv85@gmail.com>
2018-04-13 14:46:19 +0300

    add button Clear Cart; Orders from DB automatically receiving then user login;

--- project/pages/cart/saveCart.php
+++ project/pages/cart/saveCart.php
@@ -6,8 +6,12 @@ session_start();
 //получаем ID пользователя
 $user = $_SESSION['id'];
 
+//удаляем предыдущий заказ этого пользователя в БД
+query("DELETE FROM order_products WHERE customer_id = {$user}", '', BOOKS_DB);
+
+
 //преобразовываем массив с ID товара и количеством в строку, чтобы далее вставить в запрос.
-foreach ($_POST['arr'] as $key => $value) {
+foreach ($_POST['order'] as $key => $value) {
     $string[] = "($user, {$value['id']},  {$value['count']})";
 }
 
@@ -16,3 +20,5 @@ $string = implode(', ', $string);
 query("INSERT INTO order_products 
         (customer_id, product_id, count) 
 VALUES $string", null, BOOKS_DB);
+
+echo "{$_SESSION['name']}, your cart is saved!";
